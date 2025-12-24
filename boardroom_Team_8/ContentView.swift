0252407239d4a81cb8be351e42ec8be834db9574
jumana <jmanhtrfan@gmail.com>
2025-12-24 15:13:52 +0300

    Initial Commit

---
+++ boardroom_Team_8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  boardroom_Team_8
+//
+//  Created by Jumana on 04/07/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ boardroom_Team_8/boardroom_Team_8App.swift
@@ -0,0 +1,17 @@
+//
+//  boardroom_Team_8App.swift
+//  boardroom_Team_8
+//
+//  Created by Jumana on 04/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct boardroom_Team_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
